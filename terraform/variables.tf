726c6831e18a3dad81cdccf336430fee52169978
TVSEIMS <karishmawork26@gmail.com>
2026-06-16 22:10:54 +0530

    Define Terraform variables for EC2 configuration
    Added variables for region, AMI ID, instance type, and instance name.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
 
+variable "ami_id" {
+  description = "AMI ID for EC2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "github-terraform-instance"
+}
